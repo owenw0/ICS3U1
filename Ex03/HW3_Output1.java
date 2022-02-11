a326ee110d0c71a24930c6b6f04df5b97f2a7d42
Owen Wang <owenwang0099@gmail.com>
2022-02-11 14:36:12 -0500

    exercise

--- Ex03/HW3_Output1.java
+++ Ex03/HW3_Output1.java
@@ -1,5 +1,10 @@
 public class HW3_Output1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(5);
+        System.out.println("\t3");
+        System.out.println("5*2=" + 5 * 2);
+        System.out.println("My name is\tGeorge!!!!");
+        System.out.println("3.0\t10.563\tharry\n\n\n");
+        System.out.println("I think I got it!");
     }
 }
