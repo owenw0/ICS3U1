375e3a77e1d8c438c65d8f93026a11b1187c9479
Owen Wang <owenwang0099@gmail.com>
2022-03-29 13:21:12 -0400

    edit

--- Unit 2/Ex18/Factors.java
+++ Unit 2/Ex18/Factors.java
@@ -4,7 +4,7 @@ import java.util.*;
     Factors
     Owen Wang
     Last modified: 2022-03-28
-
+    Outputs all factors of a number.
 */
 public class Factors {
     public static void main(String[] args) {
