c3eb0322bd9d4fe5cadfe2da28e15d92bd907d0f
Owen Wang <owenwang0099@gmail.com>
2022-05-31 10:59:49 -0400

    edits

--- Unit 4/Main.java
+++ Unit 4/Main.java
@@ -8,83 +8,127 @@ import java.io.*;
     Budget ATM System to save banking information.
 */
 public class Main {
-    public static boolean create_profile(String pin) {
+    public static String[] retrieve_data(String num) {
         // variable declaration
-        boolean run = true, unique = false;
-        String choice, last_name, first_name, num;
+        String pin = "", last_name = "", first_name = "", acc_type = "", checkings = "", savings = "";
+        String[] data = new String[5];
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(num));
+
+            pin = br.readLine();
+            data[0] = pin;
+            last_name = br.readLine();
+            data[1] = last_name;
+            first_name = br.readLine();
+            data[2] = first_name;
+
+            acc_type = br.readLine();
+            if (acc_type == null) {
+                // no accounts open
+                checkings = "0";
+                savings = "0";
+            } else if (acc_type == "CHECKINGS") {
+                // checkings account open
+                checkings = br.readLine();
+                acc_type = br.readLine();
+                // determine if savings account also open
+                if (acc_type != null) {
+                    savings = br.readLine();
+                }
+            } else if (acc_type == "SAVINGS") {
+                // savings account open
+                savings = br.readLine();
+                acc_type = br.readLine();
+                // determine if checkings account also open
+                if (acc_type != null) {
+                    checkings = br.readLine();
+                }
+            }
+            data[3] = checkings;
+            data[4] = savings;
+        } catch (IOException e) {
+        }
+        return data;
+    }
+
+    public static String[] create_profile(String pin) {
+        // variable declaration
+        boolean unique = false, valid_pin = false;
+        String last_name, first_name, num;
+        String[] data = new String[5];
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
         BufferedWriter bw;
         BufferedReader br;
 
-        System.out.println("Customer does not exist.");
-        while (run) {
-            // prompt for whether user would like to open new profile
-            System.out.print("Would you like to open a new customer profile? (Y/N) ");
-            choice = sc.nextLine().toLowerCase();
-            switch (choice) {
-                case "y":
-                    // prompt for name
-                    System.out.print("\nEnter last name: ");
-                    last_name = sc.nextLine();
-                    System.out.print("Enter first name: ");
-                    first_name = sc.nextLine();
+        System.out.print("\nEnter last name: ");
+        last_name = sc.nextLine();
+        System.out.print("Enter first name: ");
+        first_name = sc.nextLine();
 
-                    last_name = last_name.substring(0, 1).toUpperCase() + last_name.substring(1).toLowerCase();
-                    first_name = first_name.substring(0, 1).toUpperCase() + first_name.substring(1).toLowerCase();
+        // captitalize first letter of name and lowercase rest
+        last_name = last_name.substring(0, 1).toUpperCase() + last_name.substring(1).toLowerCase();
+        first_name = first_name.substring(0, 1).toUpperCase() + first_name.substring(1).toLowerCase();
 
-                    // confirm customer number
-                    do {
-                        // generate random number
-                        num = Integer.toString(random.nextInt(100000, 999999));
-                        // determine if number is unique
-                        try {
-                            br = new BufferedReader(new FileReader(num + ".txt"));
-                            unique = true;
-                        } catch (IOException e) {
-                        }
-                    } while (unique = false);
-
-                    System.out.printf("\nYour customer number: %s\n", num);
-
-                    // confirm PIN
-                    while (pin.length() != 4) {
-                        System.out.print("Enter 4-digit PIN: ");
-                        pin = sc.nextLine();
-                    }
-                    System.out.printf("Your PIN: %s\n\n", pin);
-
-                    // write to file
-                    try {
-                        bw = new BufferedWriter(new FileWriter(num + ".txt", false));
-                        bw.write(num + "\n");
-                        bw.write(pin + "\n");
-                        bw.write(last_name + "\n");
-                        bw.write(first_name + "\n");
-                        bw.close();
-                    } catch (IOException e) {
-                        System.out.println(e);
-                    }
-                    return true;
-
-                case "n":
-                    return false;
-
-                default:
-                    System.out.println("Invalid option, try again.");
+        do {
+            // generate random customer number
+            num = Integer.toString(random.nextInt(100000, 999999));
+            // determine if number is unique
+            try {
+                br = new BufferedReader(new FileReader(num));
+                unique = true;
+            } catch (IOException e) {
             }
+        } while (unique = false);
+        System.out.printf("\nYour customer number: %s\n", num);
+
+        // confirm PIN
+        do {
+            try {
+                if (pin.length() != 4) {
+                    valid_pin = false;
+                } else {
+                    Integer.parseInt(pin);
+                    valid_pin = true;
+                }
+            } catch (NumberFormatException e) {
+                valid_pin = false;
+            }
+
+            // determine if pin is valid
+            if (valid_pin = false) {
+                System.out.println("Invalid pin.");
+                System.out.print("Enter 4-digit PIN:");
+                pin = sc.nextLine();
+            }
+        } while (!valid_pin);
+        System.out.printf("Your PIN: %s\n\n", pin);
+
+        // write to file
+        try {
+            bw = new BufferedWriter(new FileWriter(num, false));
+            bw.write(pin + "\n");
+            bw.write(last_name + "\n");
+            bw.write(first_name + "\n");
+            bw.close();
+        } catch (IOException e) {
         }
-        return true;
+
+        data[0] = pin;
+        data[1] = last_name;
+        data[2] = first_name;
+        return data;
     }
 
-    public static void open_account(String num) {
+    public static String[] open_account(String num, String[] data) {
         // variable declaration
         String acc_type = "", temp = "";
         BufferedReader br;
         BufferedWriter bw;
 
         try {
-            br = new BufferedReader(new FileReader(num + ".txt"));
+            br = new BufferedReader(new FileReader(num));
             // read through basic information
             for (int i = 0; i < 4; i++) {
                 br.readLine();
@@ -94,7 +138,7 @@ public class Main {
             if (acc_type == null) {
                 // no accounts open
                 // prompt for which account to open
-                System.out.println("Select account to open:");
+                System.out.println("\nSelect account to open:");
                 System.out.println("    1: Checkings");
                 System.out.println("    2: Savings");
             } else {
@@ -106,7 +150,7 @@ public class Main {
                     if (acc_type == "SAVINGS") {
                         System.out.println("Opening checkings account.");
                         try {
-                            bw = new BufferedWriter(new FileWriter(num + ".txt", true));
+                            bw = new BufferedWriter(new FileWriter(num, true));
                             bw.write("CHECKINGS");
                             bw.write("0");
                         } catch (IOException e) {
@@ -114,7 +158,7 @@ public class Main {
                     } else {
                         System.out.println("Opening savings account.");
                         try {
-                            bw = new BufferedWriter(new FileWriter(num + ".txt", true));
+                            bw = new BufferedWriter(new FileWriter(num, true));
                             bw.write("SAVINGS");
                             bw.write("0");
                         } catch (IOException e) {
@@ -124,9 +168,10 @@ public class Main {
             }
         } catch (IOException e) {
         }
+        return data;
     }
 
-    public static void check_balance(String num) {
+    public static void check_balance(String num, String[] data) {
         // variable declaration
         boolean run = true;
         String checkings_balance = "", savings_balance = "", open_account = "", acc_type = "";
@@ -135,8 +180,8 @@ public class Main {
         BufferedReader br;
 
         try {
-            br = new BufferedReader(new FileReader(num + ".txt"));
-            // read first 5 lines (basic customer info)
+            br = new BufferedReader(new FileReader(num));
+            // read first 5 lines (basic customer data)
             for (int i = 0; i < 5; i++) {
                 acc_type = br.readLine();
             }
@@ -149,7 +194,7 @@ public class Main {
                 switch (open_account) {
                     case "y":
                     case "yes":
-                        open_account(num);
+                        open_account(num, data);
                         break;
 
                     case "n":
@@ -213,8 +258,9 @@ public class Main {
 
     public static void main(String[] args) {
         // variable declaration
-        boolean logged_in = true, menu = true;
-        String num, pin, file_PIN, line = "", last_name, first_name;
+        boolean logged_in = true, menu = true, open_prof = false;
+        String num, pin, file_PIN, line = "", last_name, first_name, open_profile;
+        String[] data;
         int choice = 0;
         Scanner sc = new Scanner(System.in);
         BufferedReader br;
@@ -225,18 +271,18 @@ public class Main {
                 // promt for user information
                 System.out.print("Enter 6-digit customer number: ");
                 num = sc.nextLine();
+                num += ".txt";
                 System.out.print("Enter PIN: ");
                 pin = sc.nextLine();
+
                 try {
-                    br = new BufferedReader(new FileReader(num + ".txt"));
-                    line = br.readLine();
-                    // read PIN
+                    br = new BufferedReader(new FileReader(num));
+                    // confirm PIN
                     file_PIN = br.readLine();
                     if (pin.equals(file_PIN)) {
-                        // retrieve information from file
-                        last_name = br.readLine();
-                        first_name = br.readLine();
                         br.close();
+                        // retrieve data
+                        data = retrieve_data(num);
                         do {
                             // display menu
                             System.out.println("\nPress 1 to check balance.");
@@ -247,60 +293,78 @@ public class Main {
                             System.out.println("Press 6 to change PIN.");
                             System.out.println("Press 7 to logout.");
                             try {
+                                // prompt for choice
                                 choice = sc.nextInt();
                             } catch (InputMismatchException e) {
+                                // if string or float was entered
                                 System.out.println("Invalid option.");
                             }
 
-                            if (menu) {
-                                switch (choice) {
-                                    case 1:
-                                        // call check_balance method
-                                        check_balance(num);
-                                        menu = false;
-                                        break;
+                            switch (choice) {
+                                case 1:
+                                    // call check_balance method
+                                    check_balance(num, data);
+                                    menu = false;
+                                    break;
 
-                                    case 2:
-                                        // call deposit method
-                                        menu = false;
-                                        break;
+                                case 2:
+                                    // call deposit method
+                                    menu = false;
+                                    break;
 
-                                    case 3:
-                                        // call withdraw method
-                                        menu = false;
-                                        break;
+                                case 3:
+                                    // call withdraw method
+                                    menu = false;
+                                    break;
 
-                                    case 4:
-                                        // call close_account method
-                                        menu = false;
-                                        break;
+                                case 4:
+                                    // call close_account method
+                                    menu = false;
+                                    break;
 
-                                    case 5:
-                                        // call open_account method
-                                        menu = false;
-                                        break;
+                                case 5:
+                                    // call open_account method
+                                    menu = false;
+                                    break;
 
-                                    case 6:
-                                        // call change_PIN method
-                                        menu = false;
-                                        break;
+                                case 6:
+                                    // call change_PIN method
+                                    menu = false;
+                                    break;
 
-                                    case 7:
-                                        // exit out of loop and logout
-                                        menu = false;
-                                        break;
+                                case 7:
+                                    // exit out of loop and logout
+                                    menu = false;
+                                    break;
 
-                                    default:
-                                        // output error message
-                                        System.out.println("Invalid option.");
-                                }
+                                default:
+                                    // output error message if invalid number entered
+                                    System.out.println("Invalid option.");
                             }
                         } while (menu);
                     } else {
+                        // incorrect user PIN entered
                         System.out.println("Incorrect PIN.\n");
                     }
                 } catch (IOException e) {
-                    if (create_profile(pin) == false) {
+                    // file not found
+                    System.out.println("User does not exist.");
+                    System.out.print("Would you like to open a new customer profile? (Y/N) ");
+                    open_profile = sc.nextLine().toLowerCase();
+                    while (!open_prof) {
+                        switch (open_profile) {
+                            case "y", "yes":
+                                data = create_profile(pin);
+                                open_prof = true;
+                                break;
+
+                            case "n", "no":
+                                open_prof = true;
+                                break;
+
+                            default:
+                                System.out.println("Invalid option.");
+                        }
                         System.out.println();
                         logged_in = false;
                     }
